0cda19a8a8150efd55f7ba9532822cbb73632600
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-03 20:51:17 -0300

    Fix syntax error in functions and callbacks

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ console.log('Relembrando funções!')
 * 2. Criar uma função que permita listar a placa de um veículo passando um ID
 */
 
-function listarVeiculo(id, callback) {
-  console.log('Chamando a função listarVeículos');
-  setTimeout(function() {
-    return callback(null, {
-    id: 1,
-    nome: "Gol"
-    })
-  }, 1000)
+function listarVeiculo(callback){
+  console.log('Chamando a função listarVeiculo()')
+  setTimeout(function(){ return callback(null,{
+      id: 1,
+      nome:"Gol",});
+  },3000)
 }
 
-  function listarPlaca(id, callback) {
-    console.log('Chamando a função listarPlaca');
-    let tempo = 2000;
-    setTimeout(function() {
+function listarPlaca(id,callback){
+  console.log('Chamando a função listarPlaca()')
+  setTimeout(function(){
       return callback(null, {
-      id: id,
-      nome: "Gol - placa do carro",
-      placa: "XXX-YYYY"
-    })
-  }, tempo)
+          id:id,
+          nome:'Gol',
+          placa:"XXX-YXYY"
+      });
+  },4000)
 }
 
-listarVeiculo(function resolverVeiculo(erro, veiculo){
-  console.log(veiculo)
+listarVeiculo(function resolverVeiculo(erro,veiculo){
+  listarPlaca(veiculo.id, function resolverPlaca(erroPlaca,placa){
+      console.log(veiculo)
+      console.log(placa)
+  })
 })
 
 // const veiculo = listarVeiculo();
