c20c7f706745c49ec232e363b7df9f27140ddcb8
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-04 19:51:19 -0300

    Feat manipulating arrays

---
+++ manipulando-arrays.js
@@ -0,0 +1,34 @@
+console.log("Manipulando meu array.")
+
+const avengers = [
+  {
+    "nome": "Capitão América",
+    "genero": "H",
+    "idade": 84,
+    "habilidades": ["honestidade", "escudo", "porrada"]
+  },
+  {
+    "nome": "Homem de Ferro",
+    "genero": "H",
+    "idade": 30,
+    "habilidades": ["dinheiro", "inteligência", "porrada", "armadura"]
+  },
+  {
+    "nome": "Capitã Marvel",
+    "genero": "F",
+    "idade": 45,
+    "habilidades": ["porrada", "fogo", "super forte", "invencivel", "voar", "destreza"]
+  },
+  {
+    "nome": "War Machine",
+    "genero": "H",
+    "idade": 40,
+    "habilidade":["exercito", "armadura", "honestidade", "porrada"]
+  },
+  {
+    "nome": "Gavião Arqueiro",
+    "genero": "H",
+    "idade": 36,
+    "habilidade":["arco e flecha", "porrada"]
+  }
+]
