768065e2c977f8eb89367fc946ec0e56970cf97f
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-04 21:16:42 -0300

    Feat continuation of operations with arrays

--- manipulando-arrays.js
+++ manipulando-arrays.js
@@ -39,6 +39,35 @@ const masculino = avengers.filter(item => item.genero == "H")
 console.log(masculino)
 
 // Quero listar todos os nomes de heróis
-const nomes = avengers.map(item => item.nome)
+// O que eu preciso retornar de fato? 
+// Solução I
+const nomes = avengers.map(item => [item.nome,item.genero])
 
+// Solução II
+const objetoDeHerois = avengers.map(item => {
+  return {
+    nome: item.nome,
+    genero: item.genero
+  }
+})
+
+// Preciso retornar o total de idade dos heróis
+const totalIdade = avengers.reduce((total, item) => {
+  return total + item.idade
+}, 0)
+
+// Listar um avenger que possua mais de 50 anos
+const experiente = avengers.some((item) => item.idade > 50)
+
+// Saber se todos os objetos do array possuem a habilidade porrada
+const porrada = avengers.every(item => item.habilidades.includes("fogo"))
+
+// Buscar avenger por genero = F
+const buscaEspecifica = avengers.find(item => item.genero == "H")
+
+console.log(objetoDeHerois)
 console.log(nomes)
+console.log(totalIdade)
+console.log(experiente)
+console.log(porrada)
+console.log(buscaEspecifica)
